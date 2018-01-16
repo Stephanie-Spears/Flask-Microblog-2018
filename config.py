fe1f9b0998199b4a8eab3fa03e24b167ce73b0c7
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-01-15 18:21:23 -0800

    add requirements.txt, error handlers

--- config.py
+++ config.py
@@ -7,3 +7,10 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('MICROBLOG_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    MAIL_SERVER = os.environ.get('YMAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('YMAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('YMAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('YMAIL_USERNAME_ONE_ONE')
+    MAIL_PASSWORD = os.environ.get('YMAIL_PASSWORD_ONE_ONE')
+    ADMINS = [os.environ.get('YMAIL_USERNAME_ONE_ONE')]
+
