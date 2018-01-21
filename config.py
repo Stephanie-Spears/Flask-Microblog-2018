10f108a7e02e97543888b949218c00f2e887f377
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-01-21 05:07:16 -0800

    add elasticsearch for full-text search mapping

--- config.py
+++ config.py
@@ -19,32 +19,11 @@ class Config(object):
 
     ADMINS = [os.environ.get('GMAIL_USERNAME_ONE_ONE')]
 
-    # Flask-Babel language code: en = English, es = Spanish, ja=Japanese
+    # Flask-Babel language code: en = English, es = Spanish, ja=Japanese, it=Italian, de=German, fr=French
     LANGUAGES = ['en', 'es', 'ja', 'it', 'de', 'ru', 'fr']
 
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
+    #run 'elasticsearch' in terminal, then diff terminal 'flask run'
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
 
-    POSTS_PER_PAGE = 15
-
-
-
-
-# # FROM GITHUB
-
-    # from Translator.mstranslator.client import MSTranslator
-    #
-    # from Translator.mstranslator.client import MSTranslatorAccessKey
-    # # TEST FROM GITHUB CREDENTIALS
-    # client_id = '263ee1af-8087-4592-bf85-5941238bdab6'
-    # client_secret = 'jEd2T2wT0dcRaHECngyGfyplmq3PjKlZIaC1iIqfzMw='
-    # MS_TRANSLATOR_KEY = MSTranslatorAccessKey(client_id, client_secret)
-    # key = MS_TRANSLATOR_KEY
-    #
-    # SCOPE='http://api.microsofttranslator.com'
-    # OAUTH_URL='https://datamarket.accesscontrol.windows.net/v2/OAuth2-13'
-    # AZURE_TRANSLATE_API_URL='http://api.microsofttranslator.com/V2/Ajax.svc/Translate?%s'
-    # TRANSLATE_ARRAY='http://api.microsofttranslator.com/V2/Http.svc/TranslateArray'
-    # GRANT_CLIENT_CREDENTIALS_ONLY='client_credentials'
-    # YOUR_APP_KEY ='263ee1af-8087-4592-bf85-5941238bdab6'
-    # YOUR_APP_SECRET = 'jEd2T2wT0dcRaHECngyGfyplmq3PjKlZIaC1iIqfzMw='
+    POSTS_PER_PAGE = 10
--- migrations/versions/4b77b08fcb23_.py
+++ migrations/versions/4b77b08fcb23_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 163c5abfe27f
+Revision ID: 4b77b08fcb23
 Revises: 
-Create Date: 2018-01-20 20:59:45.934636
+Create Date: 2018-01-21 04:27:47.031250
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '163c5abfe27f'
+revision = '4b77b08fcb23'
 down_revision = None
 branch_labels = None
 depends_on = None
